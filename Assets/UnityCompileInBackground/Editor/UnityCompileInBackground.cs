d54cd7adfb193de5335cf2c5a41aed56e44ff5d5
baba_s <shotababa1206@gmail.com>
2018-12-29 16:27:46 +0900

    fix scripts

--- Assets/UnityCompileInBackground/Editor/UnityCompileInBackground.cs
+++ Assets/UnityCompileInBackground/Editor/UnityCompileInBackground.cs
@@ -1,96 +1,134 @@
-﻿using UnityEditor;
+﻿using System.Diagnostics;
+using UnityEditor;
+using UnityEditor.Compilation;
 using UnityEngine;
 
 namespace KoganeUnityLib
 {
-	[InitializeOnLoad]
+	/// <summary>
+	/// バックグラウンドでコンパイルを開始するためのエディタ拡張
+	/// </summary>
 	public static class UnityCompileInBackground
 	{
-		private const string NAME						= "UnityCompileInBackground";
-		private const string KEY_IS_ENABLE				= NAME + "-IsEnable";
-		private const string KEY_WAIT_TIME				= NAME + "-WaitTime";
-		private const string KEY_IS_ENABLE_DEBUG_LOG	= NAME + "-IsEnableDebugLog";
+		//==============================================================================
+		// 定数
+		//==============================================================================
+		private const string CONSOLE_APP_PATH = @"UnityCompileInBackground/Editor/UnityCompileInBackground-Watcher.exe";
 
-		private static bool		m_isEnable			;
-		private static bool		m_isEnableDebugLog	;
-		private static float	m_waitTime			;
-		private static double	m_time				;
-		private static bool		m_isLoaded			;
+		//==============================================================================
+		// 変数
+		//==============================================================================
+		private static Process	m_process	;	// ファイル監視ツールのプロセス
+		private static bool		m_isRefresh	;	// コンパイルを開始する場合 true
 
-		static UnityCompileInBackground()
+		//==============================================================================
+		// 関数
+		//==============================================================================
+		/// <summary>
+		/// Unity エディタが起動した時に呼び出されます
+		/// </summary>
+		[InitializeOnLoadMethod]
+		private static void Init()
 		{
-			m_time = EditorApplication.timeSinceStartup;
-			EditorApplication.update += OnUpdate;
-		}
+			// ファイル監視ツールを起動します
+			// ツールからメッセージを受信したらコンパイルを開始します
+			var dataPath	= Application.dataPath;
+			var filename	= dataPath + "/" + CONSOLE_APP_PATH;
+			var path		= Application.dataPath;
+			var arguments	= string.Format( @"-p ""{0}"" -w 0", path );
+			var windowStyle	= ProcessWindowStyle.Hidden;
 
-		private static void Load()
-		{
-			if ( m_isLoaded ) return;
-
-			m_isLoaded = true;
-
-			m_isEnable			= EditorPrefs.GetBool( KEY_IS_ENABLE, true );
-			m_waitTime			= EditorPrefs.GetFloat( KEY_WAIT_TIME, 1 );
-			m_isEnableDebugLog	= EditorPrefs.GetBool( KEY_IS_ENABLE_DEBUG_LOG, false );
-		}
-
-		private static void OnUpdate()
-		{
-			Load();
-
-			if ( !m_isEnable ) return;
-			if ( EditorApplication.isCompiling ) return;
-			if ( EditorApplication.isUpdating ) return;
-			if ( EditorApplication.timeSinceStartup - m_time < m_waitTime ) return;
-
-			m_time = EditorApplication.timeSinceStartup;
-			AssetDatabase.Refresh();
-
-			if ( !m_isEnableDebugLog ) return;
-
-			Debug.LogFormat( "[ {0} ] Refresh", NAME );
-		}
-
-#if !UNITY_2018_3_OR_NEWER
-		[PreferenceItem( "Compile in BG" )]
-#endif
-		private static void OnGUI()
-		{
-			Load();
-
-			EditorGUI.BeginChangeCheck();
-
-			m_isEnable			= EditorGUILayout.Toggle( "Enabled", m_isEnable );
-			m_waitTime			= MultipleFloor( EditorGUILayout.Slider( "Wait Time ( sec )", m_waitTime, 0, 10 ), 0.1f );
-			m_isEnableDebugLog	= EditorGUILayout.Toggle( "Enabled Debug Log", m_isEnableDebugLog );
-
-			if ( EditorGUI.EndChangeCheck() )
+			var info = new ProcessStartInfo
 			{
-				EditorPrefs.SetBool( KEY_IS_ENABLE, m_isEnable );
-				EditorPrefs.SetFloat( KEY_WAIT_TIME, m_waitTime );
-				EditorPrefs.SetBool( KEY_IS_ENABLE_DEBUG_LOG, m_isEnableDebugLog );
-			}
-		}
-
-#if UNITY_2018_3_OR_NEWER
-		[SettingsProvider]
-		public static SettingsProvider CreateSettingsProvider()
-		{
-			var path		= "Preferences/" + NAME;
-			var provider	= new SettingsProvider( path, SettingsScope.User )
-			{
-				label		= NAME,
-				guiHandler	= _ => OnGUI(),
-				keywords	= new []{ NAME, "Enabled", "Wait Time ( sec )" },
+				FileName				= filename		,
+				UseShellExecute			= false			,
+				RedirectStandardOutput	= true			,
+				CreateNoWindow			= true			,
+				WindowStyle				= windowStyle	,
+				Arguments				= arguments		,
 			};
 
-			return provider;
-		}
-#endif
+			m_process = Process.Start( info );
+			m_process.OutputDataReceived += OnReceived;
+			m_process.BeginOutputReadLine();
 
-		private static float MultipleFloor( float value, float multiple )
+			UnityEngine.Debug.Log( "[UnityCompileInBackground] Start Watching" );
+
+			EditorApplication.update += OnUpdate;
+			EditorApplication.quitting += OnQuit;
+			CompilationPipeline.assemblyCompilationStarted += OnCompilationStarted;
+		}
+
+		/// <summary>
+		/// Unity エディタが終了する時に呼び出されます
+		/// </summary>
+		private static void OnQuit()
 		{
-			return Mathf.Floor( value / multiple ) * multiple;
+			Dispose();
+		}
+
+		/// <summary>
+		/// コンパイルが開始した時に呼び出されます
+		/// </summary>
+		private static void OnCompilationStarted( string _ )
+		{
+			Dispose();
+		}
+
+		/// <summary>
+		/// ファイル監視ツールを止めます
+		/// </summary>
+		private static void Dispose()
+		{
+			// すでに止まっている場合は何も行いません
+			if ( m_process == null ) return;
+
+			if ( !m_process.HasExited )
+			{
+				m_process.Kill();
+			}
+			m_process.Dispose();
+			m_process = null;
+
+			UnityEngine.Debug.Log( "[UnityCompileInBackground] Stop Watching" );
+		}
+
+		/// <summary>
+		/// エディタの更新タイミングで呼び出されます
+		/// </summary>
+		private static void OnUpdate()
+		{
+			// コンパイルフラグが立っていない場合、コンパイル中の場合、
+			// リフレッシュ中の場合はここで処理を止めます
+			if ( !m_isRefresh ) return;
+			if ( EditorApplication.isCompiling ) return;
+			if ( EditorApplication.isUpdating ) return;
+
+			// コンパイルを開始します
+			UnityEngine.Debug.Log( "[UnityCompileInBackground] Start Compiling" );
+
+			m_isRefresh = false;
+
+			AssetDatabase.Refresh();
+		}
+
+		/// <summary>
+		/// ファイル監視ツールからメッセージを受信した時に呼び出されます
+		/// </summary>
+		private static void OnReceived( object sender, DataReceivedEventArgs e )
+		{
+			var message = e.Data;
+
+			// ファイルに変更があった場合もしくは
+			// ファイルの名前が変更されたらコンパイルフラグを立てます
+			//
+			// この関数では AssetDatabase.Refresh を呼び出しても何も起きないので
+			// フラグだけ立てておいて Refresh は EditorApplication.update で行います
+			if ( message.Contains( "OnChanged" ) || message.Contains( "OnRenamed" ) )
+			{
+				m_isRefresh = true;
+			}
 		}
 	}
 }
+
